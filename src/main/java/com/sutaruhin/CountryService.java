8868195aedbaca8b61ac41b339cc16e4ec00db58
shout <shout@192.168.11.4>
2023-11-05 17:22:30 +0900

    DBへの追加・更新(新規登録)・削除処理を追加

--- src/main/java/com/sutaruhin/CountryService.java
+++ src/main/java/com/sutaruhin/CountryService.java
@@ -1,6 +1,9 @@
 package com.sutaruhin;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,5 +15,25 @@ public class CountryService {
 
 	public List<Country> getCountryList(){
 		return repository.findAll();
+	}
+
+	public Country getCountry(String code) {
+//		レコードを1件検索する
+		Optional<Country> option = repository.findById(code);
+//　レコードを取得できなかったときはnullを返す
+		Country country = option.orElse(null);
+		return country;
+	}
+
+	@Transactional
+	public void updateCountry(String code,String name,int population) {
+		Country country = new Country(code,name,population);
+		repository.save(country);
+	}
+
+	@Transactional
+	public void deleteCountry(String code) {
+		repository.deleteById(code);
+
 	}
 }
